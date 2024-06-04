3a623721b6ec4a1879b188e18b992527a9bc866f
She2024 <bullybluedog@hotmail.com>
2024-06-04 20:14:14 +1000

    comments in README

--- main.py
+++ main.py
@@ -2,4 +2,40 @@ print("Hello world!")
 print(5-3)
 print("Dont forget to commit")
 
-#print("It's a comment, it won't be printed")
+# print("It's a comment, it won't be printed")
+
+# Data types
+
+# Intergers 
+
+# 4 
+# 7
+# print (9)
+#print ("9")
+#print (type(9))
+#print (type("9"))
+
+# Floats
+#5.123123
+#print(3.1415)
+#print (type(-5.63))
+#print (type("-5"))
+
+# Complex (imaginary numnbers), no worries, we wont cover these kind of numbers in the course
+#2+3J
+#print(type(4 + 7j))
+
+# Operators + - * / %
+#print(4 * 5)
+#print(15 / 3)
+#print(15 / 2)
+#print (7 % 2)
+
+#string
+"Hello world"
+'Hello'
+print("this is a string")
+
+# Sting concatenations, addition, join
+print("Hello" + "world")
+print("3" + "5")
