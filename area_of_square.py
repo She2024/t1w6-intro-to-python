de68913aa8225e6ab31aafef8bdc6f4af2151b88
She2024 <bullybluedog@hotmail.com>
2024-06-04 21:04:11 +1000

    program with inputs

---
+++ area_of_square.py
@@ -0,0 +1,8 @@
+#get the length from the user
+length = float(input("Enter the length of a square:"))
+#calculate addd store the area in a variable
+#print(length)
+#calculate and store the area in a variable
+area = length * length
+#print the calcuated area to the terminal
+print(f"The calculated area is {area}")
